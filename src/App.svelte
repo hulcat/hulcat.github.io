8a2c50f4c3f244c9413a1dca7332617abd3cb6f4
catochi-h <98567658+catochi-h@users.noreply.github.com>
2022-02-04 19:18:24 +0900

    Hello hulcat!

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	export let name;
+	const network = "juno";
+	const contractAddress = "juno1pshrvuw5ng2q4nwcsuceypjkp48d95gmcgjdxlus2ytm4k5kvz2s7t9ldx";
+	function addKeplrExtension() {
+		window.keplr.suggestToken("juno", "juno1pshrvuw5ng2q4nwcsuceypjkp48d95gmcgjdxlus2ytm4k5kvz2s7t9ldx");
+	}
+</script>
+
+<main>
+	<h1>Hell<img src="http://localhost:8080/favicon.png">   {name}!</h1>
+	<p><a href="#" on:click={addKeplrExtension}>Add keplr extension</a></p>
+</main>
+
+<style>
+	img {
+		margin-left: -8px;
+		margin-right: 10px;
+		width: 50px;
+	}
+
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: auto;
+		height: 90vh;
+    	display: flex;
+    	justify-content: center;
+    	align-items: center;
+		flex-direction: column;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	h1 {
+		color: #5BC62E;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 90;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
